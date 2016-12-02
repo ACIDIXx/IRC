c058b417ed5b6262d2ba94a9cfff9ad896641099
ACIDIXx <fribourg.pierre@live.fr>
2016-12-02 15:17:12 +0000

    1st commit
    1st commit to IRACID

---
+++ IRC/Form1.Designer.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace IRC
+{
+    partial class IRACID
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(IRACID));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.Edit = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.OK = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.error1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 259);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(376, 20);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Visible = false;
+            this.textBox1.KeyDown += new KeyEventHandler(this.textBox1_KeyDown);
+            // 
+            // Edit
+            // 
+            this.Edit.Name = "Edit";
+            this.Edit.Size = new System.Drawing.Size(61, 4);
+            // 
+            // OK
+            // 
+            this.OK.Location = new System.Drawing.Point(197, 10);
+            this.OK.Name = "OK";
+            this.OK.Size = new System.Drawing.Size(65, 22);
+            this.OK.TabIndex = 3;
+            this.OK.Text = "OK";
+            this.OK.UseVisualStyleBackColor = true;
+            this.OK.Click += new System.EventHandler(this.OK_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.AcceptsReturn = true;
+            this.textBox2.ContextMenuStrip = this.Edit;
+            this.textBox2.ForeColor = System.Drawing.Color.DarkGray;
+            this.textBox2.Location = new System.Drawing.Point(14, 10);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(177, 20);
+            this.textBox2.TabIndex = 4;
+            this.textBox2.Text = "Edit user name here";
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            this.textBox2.GotFocus += new System.EventHandler(this.textBox2_GotFocus);
+            this.textBox2.KeyDown += new KeyEventHandler(this.textBox2_KeyDown);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 52);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.Vertical;
+            this.richTextBox1.Size = new System.Drawing.Size(376, 201);
+            this.richTextBox1.TabIndex = 5;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.Visible = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(25, 64);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(126, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Waiting for connection ...";
+            this.label1.Visible = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(25, 87);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Connected !";
+            this.label2.Visible = false;
+            // 
+            // error1
+            // 
+            this.error1.AutoSize = true;
+            this.error1.Location = new System.Drawing.Point(106, 150);
+            this.error1.Name = "error1";
+            this.error1.Size = new System.Drawing.Size(183, 13);
+            this.error1.TabIndex = 8;
+            this.error1.Text = "ERROR : Can\'t connect to the server";
+            this.error1.Visible = false;
+            // 
+            // IRACID
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(400, 291);
+            this.Controls.Add(this.error1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.OK);
+            this.Controls.Add(this.textBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "IRACID";
+            this.Text = "IRACID";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+
+
+        private void textBox1_enter(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void TextBox2_GotFocus(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void textBox2_clicked(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ContextMenuStrip Edit;
+        private System.Windows.Forms.Button OK;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label error1;
+    }
+}
+
---
+++ IRC/Form1.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Sockets;
+using System.IO;
+using System.Net.NetworkInformation;
+using System.Net;
+
+namespace IRC
+{
+    public partial class IRACID : Form
+    {
+        public IRACID()
+        {
+            InitializeComponent();
+        }
+
+        
+        public void Form1_Load(object sender, EventArgs e)
+        {
+            //connect to the srv when the form is loading
+            Ping pingSender = new Ping();
+            IPAddress address = IPAddress.Loopback;
+            PingReply reply = pingSender.Send(address);
+
+            if (reply.Status == IPStatus.Success)
+            {
+                label1.Visible = true;
+
+                // use the ipaddress as in the server program
+
+                label2.Visible = true;
+                label1.Visible = false;
+                label2.Visible = false;
+                richTextBox1.Visible = true;
+            }
+            else
+            {
+                error1.Visible = true;
+            }
+   
+        }
+
+
+        private void textBox2_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                
+                string userName = textBox2.Text;
+                textBox1.Visible = true;
+                OK.Visible = false;
+                textBox2.ReadOnly = true;
+                textBox2.BackColor = Color.LightGray;
+                textBox2.Text = userName;
+                textBox2.UseWaitCursor = false;
+            }
+        }
+
+        private void textBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+               // string msg = textBox1.Text;
+                //textBox1.Text = "";
+               // richTextBox1.Text = msg;
+
+                TcpClient tcpclnt = new TcpClient();
+                tcpclnt.Connect("127.0.0.1", 8001);
+                string msg = textBox1.Text;
+                textBox1.Text = "";
+                Stream stm = tcpclnt.GetStream();
+                ASCIIEncoding asen = new ASCIIEncoding();
+                byte[] ba = asen.GetBytes(msg);
+
+                    //transmition de l'info
+                stm.Write(ba, 0, ba.Length);
+
+                byte[] bb = new byte[100];
+                int k = stm.Read(bb, 0, 100);
+
+                //reinit de la case
+                textBox1.Text = "";
+            }
+
+            // for (int i = 0; i < k; i++)
+            //    Console.Write(Convert.ToChar(bb[i]));
+            // tcpclnt.Close();
+
+
+        }
+
+        private void textBox2_GotFocus(object sender, EventArgs e)
+        {
+            textBox2.Clear();
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            textBox2.ForeColor = Color.DarkBlue;
+        }
+
+
+        public void OK_Click(object sender, EventArgs e)
+        {
+            string userName = textBox2.Text;
+            textBox1.Visible = true;
+            OK.Visible = false;
+            textBox2.ReadOnly = true;
+            textBox2.BackColor = Color.LightGray;
+            textBox2.Text = userName;
+            textBox2.UseWaitCursor = false;
+        }
+
+       
+        private void recupDiscut()
+        {
+         
+        }
+         
+        private void printScreen()
+        {
+        
+        } 
+         
+         
+         
+    }
+}
